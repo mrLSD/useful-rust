462f8cb871c923a9773ad869d70deb66afbb3b04
Evgeny Ukhanov <mrlsd@ya.ru>
2016-09-26 14:08:34 +0300

    Rust By Examples [RBE] - 1.2.1 - Debug

--- src/rbe/hello_debug.rs
+++ src/rbe/hello_debug.rs
@@ -1,3 +1,39 @@
-pub fn main() {
+// This structure cannot be printed either with `fmt::Display` or
+// with `fmt::Debug`
+#[allow(dead_code)]
+struct UnPrintable(i32);
 
+// The `derive` attribute automatically creates the implementation
+// required to make this `struct` printable with `fmt::Debug`.
+#[derive(Debug)]
+struct DebugPrintable(i32);
+
+// Derive the `fmt::Debug` implementation for `Structure`. `Structure`
+// is a structure which contains a single `i32`.
+#[derive(Debug)]
+struct Structure(i32);
+
+// Put a `Structure` inside of the structure `Deep`. Make it printable
+// also.
+#[derive(Debug)]
+struct Deep(Structure);
+
+pub fn main() {
+    // Printing with `{:?}` is similar to with `{}`.
+    println!("{:?} months in a year.", 12);
+    println!("{1:?} {0:?} is the {actor:?} name.",
+             "Slater",
+             "Christian",
+             actor="actor's");
+
+    // `Structure` is printable!
+    println!("Now {:?} will print!", Structure(3));
+
+    // The problem with `derive` is there is no control over how
+    // the results look. What if I want this to just show a `7`?
+    println!("Now {:?} will print!", Deep(Structure(7)));
+
+    println!("DebugPrintable: {:?}", DebugPrintable(100));
+    // But UnPrintable(100) invoke error:
+    // println!("UnPrintable: {:?}", UnPrintable(100));
 }
