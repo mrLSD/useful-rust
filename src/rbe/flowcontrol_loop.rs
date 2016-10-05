71364c7a4d7bbae284d0e2c722b2e14fcdf04281
Evgeny Ukhanov <mrlsd@ya.ru>
2016-10-05 14:39:20 +0300

    Rust By Examples [RBE] - loop - 7.2.1 Nesting and labels

--- src/rbe/flowcontrol_loop.rs
+++ src/rbe/flowcontrol_loop.rs
@@ -1,3 +1,5 @@
+#![allow(unreachable_code)]
+
 pub fn main() {
     let mut count = 0u32;
 
@@ -23,4 +25,28 @@ pub fn main() {
             break;
         }
     }
+    loop_nested_labeles();
+}
+
+fn loop_nested_labeles() {
+    let mut i = 0;
+    'outer: loop {
+        i += 1;
+        println!("Entered the outer loop [{}]", i);
+
+        'inner: loop {
+            println!("Entered the inner loop [{}]", i);
+            if i <= 2 {
+                continue 'outer;
+            }
+            // This would break only the inner loop
+            //break;
+
+            // This breaks the outer loop
+            break 'outer;
+        }
+        println!("This point will never be reached");
+    }
+
+    println!("Exited the outer loop");
 }
